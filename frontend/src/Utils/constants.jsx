5fad643178180ebcc4bccaf7671033ecc52f3137
amaya.niko <amaya.niko@gmail.com>
2023-09-28 07:30:59 -0300

    migracion backend

--- frontend/src/Utils/constants.jsx
+++ frontend/src/Utils/constants.jsx
@@ -1,4 +1,4 @@
-const urlBase = `http://ec2-18-218-250-8.us-east-2.compute.amazonaws.com/`
+const urlBase = `ec2-18-188-254-5.us-east-2.compute.amazonaws.com/`
 const token =
     'eyJhbGciOiJIUzUxMiJ9.eyJpZCI6IjE3MCIsImZ1bGxuYW1lIjoiTmljb2xhcyBBbWF5YSIsInVzZXJfcm9sZSI6IlJPTEVfQURNSU4iLCJzdWIiOiJhbWF5YS5uaWtvQGdtYWlsLmNvbSIsImlhdCI6MTY5NTA3NjQ4OCwiaXNzIjoiaHR0cDovL2U0LW1yLmluc3RydW1lbnRzLnMzLXdlYnNpdGUudXMtZWFzdC0yLmFtYXpvbmF3cy5jb20vIiwiZXhwIjoxNjk1OTQwNDg4fQ.fE_0PEz-Zjq6eVDcnjnFaCSH49s0eA89ovZ7H0xD_G1HzsZUkasC7o3eWWbFyWnxgQBYIYiNJK-cOEg-cAJK-Q'
 const config = {
