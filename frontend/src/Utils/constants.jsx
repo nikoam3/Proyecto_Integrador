2405d7691596451062ec6a9009191d279a3077c1
amaya.niko <amaya.niko@gmail.com>
2025-06-04 23:44:46 -0300

    correcion en front por error en merge

--- frontend/src/Utils/constants.jsx
+++ frontend/src/Utils/constants.jsx
@@ -1,4 +1,5 @@
 const urlBase = `https://back-mr-instruments-v24.onrender.com/`
+
 const token =
     'eyJhbGciOiJIUzUxMiJ9.eyJpZCI6IjYiLCJmdWxsbmFtZSI6Ik5pY29sYXMgQW1heWEiLCJ1c2VyX3JvbGUiOiJST0xFX0FETUlOIiwic3ViIjoiYW1heWEubmlrby5hZG1pbkBnbWFpbC5jb20iLCJpYXQiOjE3NDg5OTAxMzEsImlzcyI6Imh0dHA6Ly9lNC1tci5pbnN0cnVtZW50cy5zMy13ZWJzaXRlLnVzLWVhc3QtMi5hbWF6b25hd3MuY29tLyIsImV4cCI6MTc4MDUzMDEzMX0.cJW0Sd009cW1n8lC-gtHXR4M78GmIO43Md1asdcLSUDeWKH93Ba9Pgybw9NAEQlnsRitrEhIKgVmaKVgxrzyDQ';
     const config = {
