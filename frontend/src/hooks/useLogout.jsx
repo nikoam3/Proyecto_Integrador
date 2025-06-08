da2932e57ce9f47fe98cd4d8780430fe69deddd7
amaya.niko <amaya.niko@gmail.com>
2025-06-08 16:35:08 -0300

    correcion logout

--- frontend/src/hooks/useLogout.jsx
+++ frontend/src/hooks/useLogout.jsx
@@ -1,9 +1,13 @@
 import { useAuthContext } from './useAuthContext'
+import { publicRoutes } from '../Utils/routes'
+import { useNavigate } from 'react-router-dom'
 
 export const useLogout = () => {
+    const navigate = useNavigate()
     const { dispatch } = useAuthContext()
     const logout = () => {
         localStorage.removeItem('user')
+        navigate(`/${publicRoutes.home}`)
         dispatch({ type: 'LOGOUT' })
     }
     return { logout }
